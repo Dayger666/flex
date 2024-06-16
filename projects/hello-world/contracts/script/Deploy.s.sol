9f80a583bd1b5b335b231f311dde40aafe029b14
root <root@test.mega>
2024-06-16 20:38:09 +0300

    Initial commit

--- projects/hello-world/contracts/script/Deploy.s.sol
+++ projects/hello-world/contracts/script/Deploy.s.sol
@@ -7,20 +7,20 @@ import {SaysGM} from "../src/SaysGM.sol";
 contract Deploy is Script {
     function run() public {
         // Setup wallet
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 deployerPrivateKey = uint256(0xaf2dcbaff01b3ccf783c873276731d560c040bbcc82a4872352038b231840968);
         vm.startBroadcast(deployerPrivateKey);
 
         // Log address
         address deployerAddress = vm.addr(deployerPrivateKey);
         console2.log("Loaded deployer: ", deployerAddress);
 
-        address registry = 0x663F3ad617193148711d28f5334eE4Ed07016602;
+        address registry = 0x3B1554f346DFe5c482Bb4BA31b880c1C18412170;
+
         // Create consumer
         SaysGM saysGm = new SaysGM(registry);
-        console2.log("Deployed SaysHello: ", address(saysGm));
+        console2.log("Deployed SaysGM: ", address(saysGm));
 
-        // Execute
+        // Stop broadcasting
         vm.stopBroadcast();
-        vm.broadcast();
     }
 }
